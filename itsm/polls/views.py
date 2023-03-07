6f5b88e99affb0b48dc29d4d80d4d2f8ec95b8fa
itmanharry <jack_chengqian@163.com>
2023-03-07 13:26:18 +0800

    multiple file upload

--- itsm/polls/views.py
+++ itsm/polls/views.py
@@ -25,13 +25,13 @@ import os
 #         pass
 def handle_uploaded_file(file=None):
     if file:
-        # 文件存放路径
-        attr_path = os.path.join(os.path.abspath('.'), 'attachments/files')
-        if not os.path.exists(attr_path):
-            os.makedirs(attr_path)
-        file_store = os.path.join(attr_path, file.name)
+        # 文件上传路径
+        upload_path = os.path.join(os.path.abspath('.'), 'attachments/files')
+        if not os.path.exists(upload_path):
+            os.makedirs(upload_path)
+        file_to_upload = os.path.join(upload_path, file.name)
         # 执行上传
-        with open(file_store, 'wb+') as destination:
+        with open(file_to_upload, 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
     else:
--- itsm/utils.py
+++ itsm/utils.py
@@ -1,8 +1,14 @@
-def handle_uploaded_file(f=None):
-    if f is not None:
-        print('Upload a file ...')
-        with open('upload.txt', 'wb+') as destination:
-            for chunk in f.chunks():
+import os
+def handle_uploaded_file(file=None):
+    if file:
+        # 文件上传路径
+        upload_path = os.path.join(os.path.abspath('.'), 'attachments/files')
+        if not os.path.exists(upload_path):
+            os.makedirs(upload_path)
+        file_to_upload = os.path.join(upload_path, file.name)
+        # 执行上传
+        with open(file_to_upload, 'wb+') as destination:
+            for chunk in file.chunks():
                 destination.write(chunk)
     else:
         print('No file to upload ...')
