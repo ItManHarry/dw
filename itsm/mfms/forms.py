89d0272a9c80d73432a091fc7a47a8ad3ca6e51a
itmanharry <jack_chengqian@163.com>
2023-03-14 11:03:54 +0800

    model form

--- itsm/mfms/forms.py
+++ itsm/mfms/forms.py
@@ -1,9 +1,24 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Author, Book
+class CalendarWidget(forms.TextInput):
+    class Media:
+        css = {
+            'all': ('css/bootstrap.min.css', 'css/bootstrap-icons.css', 'css/jquery-confirm.min.css')
+        }
+        js = ('js/jquery-3.6.1.min.js', 'js/bootstrap.bundle.min.js', 'js/jquery-confirm.min.js')
 class AuthorForm(ModelForm):
     class Meta:
         model = Author
         fields = ['name', 'title', 'birth_date']
+        labels = {
+            'title': '称呼',
+            'name': '姓名',
+            'birth_date': '生日',
+        }
+        widgets = {
+            'birth_date': CalendarWidget
+        }
 
 class BookForm(ModelForm):
     class Meta:
